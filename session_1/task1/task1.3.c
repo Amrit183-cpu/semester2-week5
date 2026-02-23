dcbc14d5c59b9c472d458cd6e37504a2b2f149a0
Amrit183-cpu <rhrt0563@leeds.ac.uk>
2026-02-23 11:41:29 +0000

    week 5 s1 task1

--- session_1/task1/task1.3.c
+++ session_1/task1/task1.3.c
@@ -16,7 +16,17 @@
 
 int main(void) {
 
-	// complete your code here
+    int num;
+
+    printf("Enter num: ");
+    scanf("%d", &num);
+
+    if (num%3==0 && num%5==0)
+    {
+        printf("The number is divisible by 3 and 5");
+    } else {
+        printf("The number is not divisible by both 3 and 5");
+    }
 
 	return 0;
 }
